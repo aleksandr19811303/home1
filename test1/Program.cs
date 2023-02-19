58f59142b9783733cea9ae42c2851e425c4054c2
Aleksandr <shbragnikov@inbox.ru>
2023-02-19 12:17:21 +0300

    1

---
+++ test1/Program.cs
@@ -0,0 +1,6 @@
+﻿// 10 программа принимает трехзначное число, на выходе показывает вторую цифру.
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int c = a%100;
+int b = c/10;
+Console.WriteLine (b);
---
+++ test2/Program.cs
@@ -0,0 +1,12 @@
+﻿// 13 программа выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.WriteLine("введите число");
+ string a = Console.ReadLine();
+ int b = a.Length;
+ if (b<3)
+ {
+    Console.WriteLine ("NO");
+ }
+ else
+ {
+     Console.WriteLine (a[2]);
+ }
